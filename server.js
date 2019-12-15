31aa4cc653775e709c7c0e821b90d9a76479caf1
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-14 20:33:39 -0500

    Install packages & update server

--- server.js
+++ server.js
@@ -2,18 +2,29 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
+/*Dependencies*/
+const bodyParser = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+//Spin up server
+const server = app.listen(port, listening);
+//const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
+//Callback to debug
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
